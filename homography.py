fde2e1a7b7b625493fc4a94f153088f2b048cbf6
Brian Bock <bbock@terpmail.umd.edu>
2020-02-14 16:23:21 -0500

    added x,y,xp,yp inputs

--- homography.py
+++ homography.py
@@ -1,8 +1,14 @@
-import numpy as np
 
-def homography(x,y)
-	xp = [100, 200, 220, 100]
-	yp = [100, 80, 80, 200]
+# import required packages
+import numpy as np
+from mySVD import mySVD
+
+def myHomography(original_coords, desired_coords):
+	# each should be an array
+	x=[original_coords[0,0],original_coords[1,0],original_coords[2,0],original_coords[3,0]]
+	y=[original_coords[0,1],original_coords[1,1],original_coords[2,1],original_coords[3,1]]
+	xp=[desired_coords[0,0],desired_coords[1,0],desired_coords[2,0],desired_coords[3,0]]
+	yp=[desired_coords[0,1],desired_coords[1,1],desired_coords[2,1],desired_coords[3,1]]
 
 	n = 9
 	m = 8
@@ -33,7 +39,7 @@ def homography(x,y)
 			A[row,8] = yp[val]
 			val += 1
 
-	U,S,V = np.linalg.svd(A)
+	U,S,V = mySVD(A)
 
 	# Check to see if A = USV' 
 	# A_ = np.round(np.linalg.multi_dot([U,S,np.transpose(V)]))
@@ -45,11 +51,11 @@ def homography(x,y)
 
 	# reshape x into H
 	H = np.reshape(x,[3,3])
+	return H
+	#print("Homography Matrix for A")
+	#print(H)
 
-	print("Homography Matrix for A")
-	print(H)
 
-
-x = [5, 150, 150, 5]
-y = [5, 5, 150, 150]
-
+# x = [5, 150, 150, 5]
+# y = [5, 5, 150, 150]
+# myHomography(x,y)
