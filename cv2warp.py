ea71ef518c4aa1b596c5d313336132799afa2015
Brian Bock <bbock@terpmail.umd.edu>
2020-02-16 15:18:48 -0500

    rearranged code

--- cv2warp.py
+++ cv2warp.py
@@ -9,6 +9,32 @@ from myWarpPerspective import myWarpPerspective
 
 imgpath="skewed_colors.jpg"
 
+
+# Define coordinates of corners (clockwise around from the top left)
+point1=[240,11]
+point2=[557,232]
+point3=[800,800]
+point4=[0,800]
+
+
+original_coords = np.array([point1, point2, point3, point4])
+desired_coords = np.array([[0, 0], [800, 0], [800, 800], [0, 800]])
+
+
+# Homography processing to make flat image
+# cvh, status = cv2.findHomography(original_coords, desired_coords)
+# print(type(cvh))
+# print(cvh) ###
+
+# Homography processing to make flat image
+h = myHomography(original_coords, desired_coords)
+# print(type(h))
+# print(h)
+
+
+
+
+
 # Read in the skewed image
 skewed_image=cv2.imread(imgpath)
 # Check that the image exists
@@ -20,33 +46,14 @@ else:
 	print("Image '"+str(imgpath)+"' imported properly.")
 
 
-	print("Started. Please be patient - this will take about 5 seconds.")
+print("Started warp. Please be patient - this will take about 5 seconds.")
 
 
-	# Define coordinates of corners (clockwise around from the top left)
-	point1=[240,11]
-	point2=[557,232]
-	point3=[800,800]
-	point4=[0,800]
 
 
-	original_coords = np.array([point1, point2, point3, point4])
-	desired_coords = np.array([[0, 0], [800, 0], [800, 800], [0, 800]])
-
-
-	# Homography processing to make flat image
-	# cvh, status = cv2.findHomography(original_coords, desired_coords)
-	# print(type(cvh))
-	# print(cvh) ###
-
-	# Homography processing to make flat image
-	h = myHomography(original_coords, desired_coords)
-	# print(type(h))
-	# print(h)
-
-	# Warp the image
-	#cv_Homography = cv2.warpPerspective(skewed_image, h, (800, 800))
-	cv_Homography = myWarpPerspective(skewed_image, h, (800, 800))
+# Warp the image
+#cv_Homography = cv2.warpPerspective(skewed_image, h, (800, 800))
+cv_Homography = myWarpPerspective(skewed_image, h, (800, 800))
 
 
 
