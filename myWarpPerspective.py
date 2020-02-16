0e82f582f27fb130f87d032a6189f836786a3853
Brian Bock <bbock@terpmail.umd.edu>
2020-02-16 15:24:12 -0500

    cleaned up old code

--- myWarpPerspective.py
+++ myWarpPerspective.py
@@ -57,22 +57,6 @@ def myWarpPerspective(skewed_image, Homography, dimensions):
 	#Comment this out if you want to warp in reverse
 	new_image=cv2.flip(new_image, -1)
 
-	# Warp Transform Calculation
-	# for x in range (0,width):
-	# 	for y in range (0,height):
-	# 		skewx=int((H[0,0]*x+H[0,1]*y+H[0,2])/(H[2,0]*x+H[2,1]*y+H[2,2]))
-	# 		skewy=int((H[1,0]*x+H[1,1]*y+H[1,2])/(H[2,0]*x+H[2,1]*y+H[2,2]))
-
-	# 		#print("("+str(skewx)+","+str(skewy)+")")
-
-	# 		# Many of the pixels in the skewed image will be way outside the bounds of the new image
-	# 		# They don't fit in the new image so they are discarded
-	# 		# Populate the new image's pixels only with those that are in bounds
-	# 		if (skewx>=0) and (skewx<width) and (skewy>=0) and (skewy<height):
-	# 			new_image[x,y,0]=skewed_image[skewx,skewy,0]
-	# 			new_image[x,y,1]=skewed_image[skewx,skewy,1]
-	# 			new_image[x,y,2]=skewed_image[skewx,skewy,2]
-
 	# cv2.imshow("new image", new_image)
 	# cv2.waitKey(0)
 
