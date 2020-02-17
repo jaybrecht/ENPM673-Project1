806ec42c5d40c2760718aeaf2735a7101876984f
Brian Bock <bbock@terpmail.umd.edu>
2020-02-17 11:45:13 -0500

    cleanup old code

--- squareWarper.py
+++ squareWarper.py
@@ -9,13 +9,6 @@ from homography import myHomography
 from myWarpPerspective import square2Warp
 
 
-# imgpath="tucker.JPG"
-
-# # Read in the skewed image
-# square_image=cv2.imread(imgpath)
-# warped_image=cv2.imread("frame.png")
-
-
 def squareWarper(corners,frame,square_image):
 
 	width,height,depth=square_image.shape
@@ -25,32 +18,15 @@ def squareWarper(corners,frame,square_image):
 	original_coords = np.array(corners)
 	desired_coords = np.array([[0, 0], [width, 0], [width, height], [0, height]])
 
-
-	# Homography processing to make flat image
-	# cvh, status = cv2.findHomography(original_coords, desired_coords)
-	# print(type(cvh))
-	# print(cvh) ###
-
 	# Compute Homography matrix to make warped image from square
 	h = myHomography(original_coords, desired_coords)
-	# print(type(h))
-	# print(h)
-
-
 
 	print("Starting warp. Please be patient - this may take a few seconds.")
 
-	#Time the warp operation
-	#start = datetime.now()
 
 	# Warp the image
 	#cv_Homography = cv2.warpPerspective(square_image, h, (800, 800))
 	cv_Homography = square2Warp(warped_image, square_image, h, (width, height))
-
-	#end = datetime.now()
-	#warptime=end-start
-	#print("Finished warp in "+str(warptime)+" (hours:min:sec)")
-
 
 	# showing flat image
 	#cv2.imshow("Unwarped image", cv_Homography)
