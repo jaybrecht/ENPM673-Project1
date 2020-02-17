178ff7031a648cca0991c681d3155dc7e6a4c039
Justin Albrecht <jalbrec1@umd.edu>
2020-02-17 13:37:58 -0500

    improved algorithm for detecting corners

--- corners.py
+++ corners.py
@@ -1,9 +1,7 @@
 import cv2
 import numpy as np
 
-from squareWarper import squareWarper
 
-video = cv2.VideoCapture('data/data_1.mp4') 
 
 imgpath="Tucker.JPG"
 
@@ -21,63 +19,48 @@ else:
 
 while(video.isOpened()):
     ret, frame = video.read()
-    height = frame.shape[0]
-    width = frame.shape[1]
-    channels = frame.shape[2]
-    blank = np.zeros((height, width, channels), dtype = "uint8")
 
     imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     imgray= cv2.medianBlur(imgray,5)
     ret, thresh = cv2.threshold(imgray, 190, 255, cv2.THRESH_BINARY)
 
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    # Output of hierarchy: [Next, Previous, First_Child, Parent]
-    parent = []
-    child = []
-    for ind in hierarchy[0]:
-        parent.append(ind[3])
-        child.append(ind[2])
+    # remove any contours that do not have a parent or child
+    wrong_cnts = []
+    for i,h in enumerate(hierarchy[0]):
+        if h[2] == -1 or h[3] == -1:
+            wrong_cnts.append(i)
+    cnts = [c for i, c in enumerate(cnts) if i not in wrong_cnts]
 
-    new_lst = []
-    new_lst2 = []
+    # sort the contours to include only the three largest
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:3]
 
-    # Find list of points that have a parent
-    for i in range(len(hierarchy[0])):
-        if parent[i] != -1:
-            new_lst.append(i)
-            # cv2.drawContours(frame, contours, i,(0,255,0), 3)
+    tag_cnt = []
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, peri*.015, True)
+        # if the countour can be approximated by a polygon with four sides include it
+        if len(approx) == 4:
+            tag_cnt.append(approx)
 
-    # Find all points that belong to the next level in the heirarchy
-    for i in new_lst:
-        if parent[i] not in new_lst:
-            new_lst2.append(i)
+    # draw the approximate polygons on a blank image
+    height = frame.shape[0]
+    width = frame.shape[1]
+    channels = frame.shape[2]
+    blank = np.zeros((height, width, channels), dtype = "uint8")
+    cv2.drawContours(blank,tag_cnt,-1,(255,0,0), 4)
+    
+    # draw the corners on the orignal image and store the values in the list corners
+    corners = []
+    for shape in tag_cnt:
+        coords = []
+        for p in shape:
+            coords.append([p[0][0],p[0][1]])
+            cv2.circle(frame,(p[0][0],p[0][1]),5,(0,0,255),-1)
+        corners.append(coords)
 
-    coords = []
-    for i in new_lst2:
-        blank = np.zeros((height, width, channels), dtype = "uint8")
-        cv2.drawContours(blank,contours,i,(255,255,255), 4)
-        gray = cv2.cvtColor(blank,cv2.COLOR_BGR2GRAY)
-        gray = np.float32(gray)
-        num_corners = 4
-        corners = cv2.goodFeaturesToTrack(gray, num_corners, .1, 100)
-        corners = np.int0(corners)
-        contour_corner = []
-        for corner in corners:
-            x,y = corner.ravel()
-            contour_corner.append([x,y])
-        coords.append(contour_corner)
 
-    for shape in coords:
-        for corner in shape:
-            x = corner[0]
-            y = corner[1]
-            cv2.circle(frame,(x,y),5,255,-1)
-
-    #cv2.imshow("Corners",frame)
-
-    newframe=squareWarper(coords[0],frame,square_image)
-    cv2.imshow("dog!",newframe)
- 
     if cv2.waitKey(1) == ord('q'):
         break
