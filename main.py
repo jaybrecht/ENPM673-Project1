3c4d4a926d9ba5445633298b25f50c6232e98503
Justin Albrecht <jalbrec1@umd.edu>
2020-02-21 13:31:01 -0500

    Added a few more user inputs (write_to_video, show_contours) fixed bug that caused crash on last frame of video

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from functions import*
 from cube import*
 import time
 
-video = cv2.VideoCapture('data/data_2.mp4') 
+write_to_video = False
+show_contours = False
+Dog_mode = False
+Cube_mode = True
+video_src = 1 # 1 for data1, 2 for data2, 3 for data3
 
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# today = time.strftime("%Y%m%d-%H%M%S")
-# fps_out = 29
-# out = cv2.VideoWriter(str(today)+".avi", fourcc, fps_out, (1920, 1080))
+if write_to_video:
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    today = time.strftime("%Y%m%d-%H%M%S")
+    fps_out = 29
+    out = cv2.VideoWriter(str(today)+".avi", fourcc, fps_out, (1920, 1080))
+    print("Writing to Video, Please Wait")
 
 K=np.array([[1406.08415449821,0,0],
            [2.20679787308599, 1417.99930662800,0],
@@ -18,78 +24,80 @@ K=np.array([[1406.08415449821,0,0],
 tag_ids = ['0101','0111','1111']
 img_paths = ['data/Tucker.jpg','data/Hailey.jpg','data/Tessa.jpg']
 
-# print("Writing to Video, Please Wait")
+video = cv2.VideoCapture('data/data_'+str(video_src)+'.mp4') 
 
-show_contours = False
-Dog_mode = True
-Cube_mode = True
-
-start_frame = 1
+start_frame = 600
 count = start_frame
 video.set(1,start_frame)
+
 while(video.isOpened()):
     print("Current frame:" + str(count))
     count += 1
     ret, frame = video.read()
-    #find correct contours
-    [all_cnts,cnts] = findcontours(frame,180)
-    #approximate quadralateral to each contour and extract corners
-    [tag_cnts,corners] = approx_quad(cnts)
-    if show_contours == True:
-        cv2.drawContours(frame,all_cnts,-1,(0,255,0), 4)
-        cv2.drawContours(frame,tag_cnts,-1,(255,0,0), 4)
+    if ret:
+        #find correct contours
+        [all_cnts,cnts] = findcontours(frame,180)
+        #approximate quadralateral to each contour and extract corners
+        [tag_cnts,corners] = approx_quad(cnts)
+        if show_contours == True:
+            cv2.drawContours(frame,all_cnts,-1,(0,255,0), 4)
+            cv2.drawContours(frame,tag_cnts,-1,(255,0,0), 4)
 
-    flag = False
+        flag = False
 
-    for i,tag in enumerate(corners):
-        #compute homography
-        dim = 200
-        H = homography(tag,dim)
-        H_inv = np.linalg.inv(H)
-        
-        #get squared tile
-        square_img = warp(H_inv,frame,dim,dim)
-        imgray = cv2.cvtColor(square_img, cv2.COLOR_BGR2GRAY)
-        ret, square_img = cv2.threshold(imgray, 180, 255, cv2.THRESH_BINARY)
-        
-        #encode squared tile
-        [tag_img,id_str,orientation] = encode_tag(square_img)
-        
-        #pick image based on id
-        if id_str in tag_ids:
-            index = tag_ids.index(id_str)
-            new_img = cv2.imread(img_paths[index])
-        else:
-            continue
-
-        #rotate image to reflect tag orientation
-        rotated_img = rotate_img(new_img,orientation)
-
-        if Dog_mode:
-            #superimpose the image on the tag
-            dim = rotated_img.shape[0]
+        for i,tag in enumerate(corners):
+            #compute homography
+            dim = 200
             H = homography(tag,dim)
-            h = frame.shape[0] 
-            w = frame.shape[1]
-            frame1 = warp(H,rotated_img,h,w)
-            frame2 = blank_region(frame,tag_cnts[i],0)
-            frame = cv2.bitwise_or(frame1,frame2)
-            flag = True
-
-        if Cube_mode:
-        # Find new cube points and draw on image
-            H=homography(tag,200)
             H_inv = np.linalg.inv(H)
-            P=projection_mat(K,H_inv)
-            new_corners=cubePoints(tag, H, P, 200)
-            face_color = (100, 100, 100) 
-            edge_color = (0, 0, 0) 
-            frame=drawCube(tag, new_corners,frame,face_color,edge_color,flag)
+            
+            #get squared tile
+            square_img = warp(H_inv,frame,dim,dim)
+            imgray = cv2.cvtColor(square_img, cv2.COLOR_BGR2GRAY)
+            ret, square_img = cv2.threshold(imgray, 180, 255, cv2.THRESH_BINARY)
+            
+            #encode squared tile
+            [tag_img,id_str,orientation] = encode_tag(square_img)
+            
+            #pick image based on id
+            if id_str in tag_ids:
+                index = tag_ids.index(id_str)
+                new_img = cv2.imread(img_paths[index])
+            else:
+                continue
 
-    cv2.imshow("Frame",frame)
+            #rotate image to reflect tag orientation
+            rotated_img = rotate_img(new_img,orientation)
 
-    # out.write(frame)
-    if cv2.waitKey(1) == ord('q'):
+            if Dog_mode:
+                #superimpose the image on the tag
+                dim = rotated_img.shape[0]
+                H = homography(tag,dim)
+                h = frame.shape[0] 
+                w = frame.shape[1]
+                frame1 = warp(H,rotated_img,h,w)
+                frame2 = blank_region(frame,tag_cnts[i],0)
+                frame = cv2.bitwise_or(frame1,frame2)
+                flag = True
+
+            if Cube_mode:
+            # Find new cube points and draw on image
+                H=homography(tag,200)
+                H_inv = np.linalg.inv(H)
+                P=projection_mat(K,H_inv)
+                new_corners=cubePoints(tag, H, P, 200)
+                face_color = (100, 100, 100) 
+                edge_color = (0, 0, 0) 
+                frame=drawCube(tag, new_corners,frame,face_color,edge_color,flag)
+
+        cv2.imshow("Frame",frame)
+
+        if write_to_video:
+            out.write(frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    else:
         break
 
-# out.release()
+if write_to_video:
+    out.release()
